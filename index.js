f37464bc5a02ff9544326fc4e59a81140ab2dfd9
khadizaliza <khadizaakter19836@gmail.com>
2022-06-11 19:32:07 +0600

    comment added

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ async function run(){
 run().catch(console.dir);
 
 app.get('/', (req, res) => {
-  res.send('Hello from tools manufaturer!')
+  res.send('Hello from tools manufaturer website!')
 })
 
 app.listen(port, () => {
